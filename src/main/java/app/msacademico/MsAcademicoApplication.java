387cf2025e286fde7613fa5b52e3b6e32fee631e
eduardaleall <eduarda.leal@ucsal.edu.br>
2026-06-04 13:26:00 -0300

    primeiro commit

---
+++ src/main/java/app/msacademico/MsAcademicoApplication.java
@@ -0,0 +1,13 @@
+package app.msacademico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAcademicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAcademicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/msacademico/MsAcademicoApplicationTests.java
@@ -0,0 +1,13 @@
+package app.msacademico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAcademicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
